88594474c661d4e4bbbcf530253089299b0503d3
C0220H1-DAUTUANPHUONG <daut.phuong@gmail.com>
2020-03-01 19:28:37 +0700

    'Add'

---
+++ module1/#3CauTrucDieuKien/ThucHanh/LuyenTap_switch_case.js
@@ -0,0 +1,37 @@
+function Bai1() {
+    let browser =prompt('Please type your ?');
+    switch (browser) {
+        case 'Edge':
+            alert("You 've got the Edge!");break;
+        case 'Chrome':
+        case 'FireFox':
+        case 'Safari':
+        case 'Opera':
+            alert('Okay we support these browser too');break;
+        default:
+            alert('We hope that this pge look ok!');
+    }
+}
+
+function Bai2() {
+    let a = prompt('a?');
+    switch (a*1) {
+        case 0:
+            alert(0);break;
+        case 1:
+            alert(1);break;
+        case 2:
+        case 3:
+            alert('2,3');break;
+    }
+}
+
+function Bai3() {
+    let value = prompt('Type a number');
+    switch ((value>0)?1:(value<0)?-1:0) {
+        case 1 : alert(1);break;
+        case -1: alert(-1);break;
+        default : alert(0);
+    }
+
+}
