323dcf5ac57d051d0322ce1c4dca77a5f99fb2bd
C0220H1-DAUTUANPHUONG <daut.phuong@gmail.com>
2020-04-16 08:47:07 +0700

    'add'

--- module2/#4.KeThua/src/BaiTap/Triangle.java
+++ module2/#4.KeThua/src/BaiTap/Triangle.java
@@ -1,7 +1,7 @@
 package BaiTap;
 
-class Shape{
-    private String color="red";
+class Shape {
+    private String color = "red";
 
     public Shape() {
     }
@@ -19,18 +19,19 @@ class Shape{
     }
 
 
-
     @Override
     public String toString() {
         return "A Shape with color of "
                 + getColor();
     }
 }
-public class Triangle extends Shape{
-    private double side1=1;
-    private double side2=1;
-    private double side3=1;
-    public Triangle(){
+
+public class Triangle extends Shape {
+    private double side1 = 1;
+    private double side2 = 1;
+    private double side3 = 1;
+
+    public Triangle() {
     }
 
     public double getSide1() {
@@ -57,29 +58,31 @@ public class Triangle extends Shape{
         this.side3 = side3;
     }
 
-    public Triangle(float side1, float side2, float side3,String color){
+    public Triangle(float side1, float side2, float side3, String color) {
         super(color);
-        this.side1=side1;
-        this.side2=side2;
-        this.side3=side3;
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
     }
-    public double getArea(){
-        double h=Math.sqrt(side1*side1-side2*side2/4);
-        return side2*h/2;
+
+    public double getArea() {
+        double h = Math.sqrt(side1 * side1 - side2 * side2 / 4);
+        return side2 * h / 2;
     }
-    public double getPerimeter(){
-        return getSide1()+getSide2()+getSide3();
+
+    public double getPerimeter() {
+        return getSide1() + getSide2() + getSide3();
     }
 
     @Override
     public String toString() {
-        return "Triangle Area:"+getArea()+"and Perimeter:"+getPerimeter()+", "+super.toString();
+        return "Triangle Area:" + getArea() + "and Perimeter:" + getPerimeter() + ", " + super.toString();
     }
 
     public static void main(String[] args) {
-        Shape shape=new Shape();
+        Shape shape = new Shape();
         System.out.println(shape);
-        Triangle triangle=new Triangle(3,4,5,"blue");
+        Triangle triangle = new Triangle(3, 4, 5, "blue");
         System.out.println(triangle);
     }
 }
