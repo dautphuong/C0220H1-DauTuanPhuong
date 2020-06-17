c3ffd5ad1779726ba3952db9498612b3544f2fe5
dautphuong <57697417+dautphuong@users.noreply.github.com>
2020-06-17 19:39:01 +0700

    fix

---
+++ module2/#16.SpringDataRepository/ThucHanh/province/src/main/java/com/codegym/province/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.codegym.province.repository;
+
+
+import com.codegym.province.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CustomerRepository extends PagingAndSortingRepository {
+
+}
