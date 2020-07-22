dafaeaa9e6383d52dd7ce2179a758157606a93e8
dautphuong <57697417+dautphuong@users.noreply.github.com>
2020-07-22 11:48:41 +0700

    fix

---
+++ module3/#6.AugularComponent&Template/ThucHanh/nameCardV2/src/app/card/card.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+
+  @Input() cardName: string;
+  @Input() email: string;
+  @Input() phone: string;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
