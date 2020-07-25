d5d22dbd59a338a92a1574b33c724869ca951cc1
dautphuong <57697417+dautphuong@users.noreply.github.com>
2020-07-25 07:09:27 +0700

    fix

--- casestudy/module3/furama-resort/src/app/app.module.ts
+++ casestudy/module3/furama-resort/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreateKhachHangComponent } from './create-khach-hang/create-khach-hang.component';
-import { ListKhachHangComponent } from './list-khach-hang/list-khach-hang.component';
-import { ListNhanVienComponent } from './list-nhan-vien/list-nhan-vien.component';
-import { ListDichVuComponent } from './list-dich-vu/list-dich-vu.component';
-import { CreateDichVuComponent } from './create-dich-vu/create-dich-vu.component';
-import { CreateHopDongComponent } from './create-hop-dong/create-hop-dong.component';
-import { CreateHopDongChiTietComponent } from './create-hop-dong-chi-tiet/create-hop-dong-chi-tiet.component';
-import { CreateNhanVienComponent } from './create-nhan-vien/create-nhan-vien.component';
-import { RegisterDichVuComponent } from './register-dich-vu/register-dich-vu.component';
-import { ReportKhachHangComponent } from './report-khach-hang/report-khach-hang.component';
+import { CreateKhachHangComponent } from './khach-hang/create-khach-hang/create-khach-hang.component';
+import { ListKhachHangComponent } from './khach-hang/list-khach-hang/list-khach-hang.component';
+import { ListNhanVienComponent } from './nhan-vien/list-nhan-vien/list-nhan-vien.component';
+import { ListDichVuComponent } from './dich-vu/list-dich-vu/list-dich-vu.component';
+import { CreateDichVuComponent } from './dich-vu/create-dich-vu/create-dich-vu.component';
+import { CreateHopDongComponent } from './hop-dong/create-hop-dong/create-hop-dong.component';
+import { CreateHopDongChiTietComponent } from './hop-dong/create-hop-dong-chi-tiet/create-hop-dong-chi-tiet.component';
+import { CreateNhanVienComponent } from './nhan-vien/create-nhan-vien/create-nhan-vien.component';
+import { RegisterDichVuComponent } from './dich-vu/register-dich-vu/register-dich-vu.component';
+import { ReportKhachHangComponent } from './khach-hang/report-khach-hang/report-khach-hang.component';
 
 @NgModule({
   declarations: [
